6b12f005649f8395a3bb43de074c219232f4cd42
Vladislav Kozhevnikov <vladislavkoj@gmail.com>
2018-08-23 17:46:43 +0300

    Написаны тесты для работы разных функция, реализована функция подсчета статистики

--- js/data/utils.test.js
+++ js/data/utils.test.js
@@ -1,11 +1,33 @@
-// Управление жизнями игрока livesControl
-// Переключение уровней levelChange
-// Отсчёт времени timeFlow
-
 import {assert} from 'chai';
 
-describe(`Stats Calculation function`, () => {
-  it(`text`, () => {
-    assert.deepEqual(statsCalc(null, 3), -1);
+const timerCalc = (time) => time++;
+timerCalc();
+// const timeFlow = (time) => {
+//   for (let i = 0; i <= 30; i++) {
+//     setTimeout(timerCalc(i), 1000);
+//   }
+//   if (time > 30) {
+//     return -1;
+//   }
+//   return time;
+// };
+
+const livesControl = (answer, lives) => {
+  if (answer !== true) {
+    lives--;
+  }
+  return lives;
+};
+
+describe(`Lives Calculation function`, () => {
+  it(`Should work and return correct data`, () => {
+    assert.deepEqual(livesControl(true, 3), 3);
+    assert.deepEqual(livesControl(false, 3), 2);
+    assert.deepEqual(livesControl(false, 1), 0);
+  });
+  it(`Should work with broken data`, () => {
+    assert.deepEqual(livesControl(undefined, 3), 2);
+    assert.deepEqual(livesControl(null, 3), 2);
+    assert.deepEqual(livesControl([], 1), 0);
   });
 });
